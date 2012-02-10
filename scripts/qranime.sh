cc34babf2f59963484a05c2f67d1bed00fbec513
Nathan Freitas <nathan@freitas.net>
2012-02-09 23:42:50 -0500

    added in script and samples

---
+++ scripts/qranime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+i=1
+filename=$1
+
+while read line; do
+    /usr/bin/qrencode -s 5 -o $filename$i.png "#$i: $line"
+    i=`expr $i + 1`	
+done < "$filename"
+
+convert -delay 25 *.png -resize 240x240 -colors 2 $filename.gif
+rm *.png
+
